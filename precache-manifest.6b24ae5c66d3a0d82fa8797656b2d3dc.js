f973bd1204c3208964e4cb5f1abd8b3ece77d2d3
Balikis-omolewa <106351278+Balikis-omolewa@users.noreply.github.com>
2022-08-24 07:40:07 -0700

    Updates

---
+++ precache-manifest.6b24ae5c66d3a0d82fa8797656b2d3dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "debdbaa63f113e70b439",
+    "url": "/my-translator-app/static/css/main.6063ced1.chunk.css"
+  },
+  {
+    "revision": "debdbaa63f113e70b439",
+    "url": "/my-translator-app/static/js/main.003e42b4.chunk.js"
+  },
+  {
+    "revision": "cc03ea46976d2f84e5a1",
+    "url": "/my-translator-app/static/js/runtime~main.0a1cd556.js"
+  },
+  {
+    "revision": "33b463733b0db9e555b2",
+    "url": "/my-translator-app/static/js/2.fbb7a266.chunk.js"
+  },
+  {
+    "revision": "20fc82b386070598cdc5",
+    "url": "/my-translator-app/static/js/3.277710fd.chunk.js"
+  },
+  {
+    "revision": "ee40d9436caccd76827af5194e49e989",
+    "url": "/my-translator-app/index.html"
+  }
+];
