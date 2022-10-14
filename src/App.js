bdd2201a0f6a341f59148e2424dba9ef2637365e
Balikis-omolewa <106351278+Balikis-omolewa@users.noreply.github.com>
2022-10-14 02:04:47 -0700

    update submit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
 import './App.css';
+import React from 'react';
 import Translate from './components/Translate';
 
 
 
 function App() {
   return (
+
     <div>
       <Translate />
     </div>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -12,9 +12,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-<BrowserRouter basename={window.location.pathname || ''}>
-  <Route exact path="/" component={Index} />
-</BrowserRouter>
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
